2169f71a8897613692219f3f8276e312730b049c
Sulav Dwivedi <sulavdwivedi@gmail.com>
2024-05-19 12:50:34 +0545

    first commit

---
+++ Ecommerce/backApi/index.jsx
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello I am Live");
+});
+
+app.listen(port, () => {
+  console.log("I am live again");
+});
